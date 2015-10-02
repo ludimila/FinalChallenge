5ae51b3f24ece24398752a5a6e53166a9164c2b5
MatheusGodinho <matheus.godinho@outlook.com>
2015-10-02 17:13:48 -0300

    Implementando o UserDAO como singleton

--- SearchBuddy/UserDAO.swift
+++ SearchBuddy/UserDAO.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class UserDAO: SBDAO {
-
+    static private var instance: UserDAO?
+    
+    var currentUser: User!
+    
+    override init () {
+        NSException(name: "Singleton", reason: "Use UserDAO.sharedInstance()", userInfo: nil).raise()
+    }
+    
+    private init(singleton: Bool!) {
+        super.init()
+    }
+    
+    static func sharedInstance() -> UserDAO {
+        if instance == nil {
+            instance = UserDAO(singleton: true)
+        }
+        return instance!
+    }
+    
 }
