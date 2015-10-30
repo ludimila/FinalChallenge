011aa0545d66d8509c44dd4881a3b345fb9e80e5
MatheusGodinho <matheus.godinho@outlook.com>
2015-10-30 19:30:04 -0200

    Buscando os dados para o facebook

--- SearchBuddy/FacebookParse.swift
+++ SearchBuddy/FacebookParse.swift
@@ -39,32 +39,35 @@ class FacebookParse: NSObject {
         }
     }
     
-    internal func getFBUserData() -> Void {
+    class func getFBUserData() -> Dictionary<String,String> {
+        var userData = [String:String]()
         if((FBSDKAccessToken.currentAccessToken()) != nil){
-            
             FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email, friends"]).startWithCompletionHandler({ (connection, result, error) -> Void in
                 if error == nil {
+                    userData["name"] = result.objectForKey("name") as? String
+                    userData["email"] = result.objectForKey("email") as? String
+                    let picture = result.objectForKey("picture") as! NSDictionary
+                    let data = picture.objectForKey("data") as! NSDictionary
+                    let url = data.objectForKey("url") as! String
+                    userData["photoUrl"] = url
                     
                     
-//                    var idFB = result.objectForKey("id") as! String
+                    userData["error"] = "nil"
+                    
 //                    // Get the user's profile picture.
-//                    var picture = result.objectForKey("picture") as! NSDictionary
-//                    var data = picture.objectForKey("data") as! NSDictionary
-//                    var url = data.objectForKey("url") as! String
-//                    
 //                    var pictureURL : NSURL = NSURL(string: url)!
 //                    var pictureImage = UIImage(data: NSData(contentsOfURL: pictureURL)!)
-//                    
-//                    
 //                    var filePathToWrite = "\(self.paths)/\(idFB)"
 //                    var imageData: NSData = UIImagePNGRepresentation(pictureImage!)!
 //                    self.fileManager.createFileAtPath(filePathToWrite, contents: imageData, attributes: nil)
                     
                 } else {
-                    print("\(error)")
+                    userData["error"] = "\(error.description)"
                 }
             })
         }
+        
+        return userData
     }
 
 }
