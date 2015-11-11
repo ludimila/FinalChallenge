26aeda49ca37e1cba692fed595324605fe0b85dc
MatheusGodinho <matheus.godinho@outlook.com>
2015-11-11 16:26:05 -0200

    Refatorando a config

--- SearchBuddy/ConfigVC.swift
+++ SearchBuddy/ConfigVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ConfigVC: UITableViewController {
 
+    var enableCell: UITableViewCell = UITableViewCell()
+    var rangeCell: UITableViewCell = UITableViewCell()
     var loginCell: UITableViewCell = UITableViewCell()
     var logoutCell: UITableViewCell = UITableViewCell()
     var deleteCell: UITableViewCell = UITableViewCell()
@@ -17,9 +19,19 @@ class ConfigVC: UITableViewController {
     var loginText: UILabel = UILabel()
     var logoutText: UILabel = UILabel()
     var deleteText: UILabel = UILabel()
+    var rangeText: UILabel = UILabel()
+    var enableText: UILabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.enableText = UILabel(frame: CGRectInset(self.loginCell.contentView.bounds, 15, 0))
+        self.enableText.text = "Enable Notification"
+        self.enableCell.addSubview(self.enableText)
+        
+        self.rangeText = UILabel(frame: CGRectInset(self.loginCell.contentView.bounds, 15, 0))
+        self.rangeText.text = "Edit range"
+        self.rangeCell.addSubview(self.rangeText)
         
         self.loginText = UILabel(frame: CGRectInset(self.loginCell.contentView.bounds, 15, 0))
         self.loginText.text = "Login"
@@ -46,63 +58,90 @@ class ConfigVC: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        switch(section){
+        case 0:
+            return 2
+        case 1:
+            return 2
+        default:
+            return 0
+        }
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         print(indexPath.row)
-        switch(indexPath.row) {
-                case 0:
-                    if UserDAO.isLogged() {
-                        return logoutCell
-                    }else{
-                        return loginCell
-                    }
-                case 1:
-                    if UserDAO.isLogged() == false{
-                        self.deleteCell.hidden = true
-                    }
-                    else{
-                        self.deleteCell.hidden = false
-                    }
-                    return deleteCell
-                
-                default: fatalError("Unknown row in section 0")
+        switch (indexPath.section){
+        case 0:
+            switch(indexPath.row){
+            case 0:
+                return enableCell
+            case 1:
+                return rangeCell
+            default: fatalError("Unknown row in section 0")
             }
+        case 1:
+            switch(indexPath.row) {
+            case 0:
+                if UserDAO.isLogged() {
+                    return logoutCell
+                }else{
+                    return loginCell
+                }
+            case 1:
+                if UserDAO.isLogged() == false{
+                    self.deleteCell.hidden = true
+                }
+                else{
+                    self.deleteCell.hidden = false
+                }
+                return deleteCell
+                
+            default: fatalError("Unknown row in section 1")
+            }
+            
+        default: fatalError("Unknown section in tableView")
+        }
     }
+    
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
-        switch(indexPath.row) {
+        switch(indexPath.section){
         case 0:
-            if UserDAO.isLogged() {
-                print("LOGUI AUTI")
-                UserDAO.userLogout()
-                tableView.reloadData()
-            }else{
-                print("LOGUI IN")
-                performSegueWithIdentifier("toLogin", sender: self)
-            }
+            print("NOP")
         case 1:
-            let alert = UIAlertController(title: "CUIDADO", message: "Você tem certeza que deseja deletar todos os seus dados?", preferredStyle: .Alert)
-            let action = UIAlertAction(title: "OK", style: .Default) { _ in
-                self.callDeleteFunction()
-                self.tableView!.reloadData()
+            switch(indexPath.row) {
+            case 0:
+                if UserDAO.isLogged() {
+                    print("LOGUI AUTI")
+                    UserDAO.userLogout()
+                    tableView.reloadData()
+                }else{
+                    print("LOGUI IN")
+                    performSegueWithIdentifier("toLogin", sender: self)
+                }
+            case 1:
+                let alert = UIAlertController(title: "CUIDADO", message: "Você tem certeza que deseja deletar todos os seus dados?", preferredStyle: .Alert)
+                let action = UIAlertAction(title: "OK", style: .Default) { _ in
+                    self.callDeleteFunction()
+                    self.tableView!.reloadData()
+                }
+                let action2 = UIAlertAction(title: "Cancelar", style: .Default){ _ in
+                    print("cancela tudo carai")
+                }
+                alert.addAction(action)
+                alert.addAction(action2)
+                self.presentViewController(alert, animated: true, completion: {})
+                
+            default: fatalError("Unknown row in section 0")
             }
-            let action2 = UIAlertAction(title: "Cancelar", style: .Default){ _ in
-                print("cancela tudo carai")
-            }
-            alert.addAction(action)
-            alert.addAction(action2)
-            self.presentViewController(alert, animated: true, completion: {})
-
         default: fatalError("Unknown row in section 0")
         }
-    }
+         }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "toLogin"{
