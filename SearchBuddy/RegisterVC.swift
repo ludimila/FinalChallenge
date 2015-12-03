38b2a5b0dbaaa085e7aea6a5e8aa0b1c12b8de61
MatheusGodinho <matheus.godinho@outlook.com>
2015-12-03 17:29:27 -0200

    Criando o cadastro novamente

--- SearchBuddy/RegisterVC.swift
+++ SearchBuddy/RegisterVC.swift
@@ -8,108 +8,49 @@
 
 import UIKit
 
-class RegisterVC: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class RegisterVC: UIViewController{
 
-    @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var usernameField: UITextField!
-    @IBOutlet weak var passwordField: UITextField!
-    @IBOutlet weak var statePicker: UIPickerView!
-    @IBOutlet weak var cityPicker: UIPickerView!
+    @IBOutlet weak var fieldName: UITextField!
+    @IBOutlet weak var fieldUser: UITextField!
+    @IBOutlet weak var fieldPassword: UITextField!
     
     var tableViewToReload:UITableView?
     
-    var stateArray: [String] = [String]()
-    var cityArray: [String] = [String]()
-    
-    var selectedCity: String!
-    var selectedState: String!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        stateArray = ["DF","GO","MG","SP","BA","RJ"]
-        cityArray = ["Cidade A","Cidade B","Cidade C","Cidade D","Cidade E"]
-        
-        statePicker.dataSource = self
-        statePicker.delegate = self
-        
-        cityPicker.dataSource = self
-        cityPicker.delegate = self
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     // The number of columns of data
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    // The number of rows of data
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        var count = 0
-        
-        if pickerView.tag == 0{
-            count = stateArray.count
-        }
-        else{
-            count = cityArray.count
-        }
-        
-        return count
-        
-    }
-    
-    // The data to return for the row and component (column) that's being passed in
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        var dataForRow = ""
-        if pickerView.tag == 0{
-            dataForRow = stateArray[row]
-        }
-        else{
-            dataForRow = cityArray[row]
-        }
-        
-        return dataForRow
-    }
-    
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView.tag == 0{
-            selectedState = stateArray[row]
-        }else{
-            selectedCity = cityArray[row]
-        }
-    }
-    
     
     @IBAction func doRegister(sender: AnyObject) {
         let user = User()
-        user.name = nameField.text
-        user.username = usernameField.text
-        user.password = passwordField.text
-//        user.city = selectedCity
-//        user.state = selectedState
+        user.name = fieldName.text
+        user.username = fieldUser.text
+        user.password = fieldPassword.text
         
         UserDAO.signUpUser(user) { (sucessed,error) -> Void in
             if sucessed{
-
                 let alert = UIAlertController(title: "Cadastro Realizado", message: "Bem-vindo \(user.name). Seu cadastro foi realizado com sucesso", preferredStyle: .Alert)
                 let action = UIAlertAction(title: "OK", style: .Default) { _ in
                     self.tableViewToReload?.reloadData()
-                    let profileController = UIStoryboard(name: "Profile", bundle: nil).instantiateInitialViewController() as! ViewController
-                    self.presentViewController(profileController, animated: false, completion: nil)
+                    if self.navigationController == nil{
+                            let storyBoard = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+                            self.presentViewController(storyBoard!, animated: true, completion: nil)
+                    }else{
+                        let tabBar = self.navigationController?.parentViewController as! UITabBarController
+                        tabBar.selectedIndex = 1
+                    }
                 }
                 alert.addAction(action)
                 self.presentViewController(alert, animated: true, completion: {})
             }else{
                 let errorString = error!.userInfo["error"] as? NSString
-                let alert = UIAlertController(title: "Cadastro Realizado", message: "Erro: \(errorString). Seu cadastro foi realizado com sucesso", preferredStyle: .Alert)
+                let alert = UIAlertController(title: "Falha no cadastro", message: "Erro: \(errorString!)", preferredStyle: .Alert)
                 let action = UIAlertAction(title: "OK", style: .Default) { _ in }
                 alert.addAction(action)
+                self.presentViewController(alert, animated: true, completion: {})
             }
         }
     }
-
 }
